013e90de728e8efdb65806667126f8a20b49acc3
Junhwan Lee <105141262+junans0boi@users.noreply.github.com>
2025-08-05 17:37:52 +0900

    chore: initial project structure

---
+++ src/test/java/com/hollywood/sweetspot/SweetspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollywood.sweetspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
