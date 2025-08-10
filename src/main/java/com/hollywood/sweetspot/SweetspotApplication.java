e0eff7d68bc635f1904275adba575716a24dfb0f
junans0boi <junans0boi@gmail.com>
2025-08-10 16:33:09 +0900

    chore: initialize Spring Boot project skeleton

---
+++ src/main/java/com/hollywood/sweetspot/SweetspotApplication.java
@@ -0,0 +1,13 @@
+package com.hollywood.sweetspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetspotApplication.class, args);
+	}
+
+}
