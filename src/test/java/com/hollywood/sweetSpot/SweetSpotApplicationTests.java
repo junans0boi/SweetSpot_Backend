088e0d54f3f03f77542db87ea34cd605cc7cce0f
junans0boi <junans0boi@gmail.com>
2025-09-09 21:02:41 +0900

    chore: Spring Initializr를 통한 초기 프로젝트 구조

---
+++ src/test/java/com/hollywood/sweetSpot/SweetSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollywood.sweetSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
